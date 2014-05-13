a6859ab18a8c055029c29d194d20df05cb60bbdc
iluxonchik <iluxon4ik@hotmail.com>
2014-05-13 22:34:42 +0100

    Chapter 3 Content Until Page 77.
    Added Chapter's 3 content until the page 77.

---
+++ ProC#5/Chapter 3/SimpleCSharpApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpApp
+{
+    // The clas that defines the Main() method is termed the application object
+    class Program // This class can be renamed
+    {
+        //the Main() method is the entry point of the application
+        static int Main(string[] args)
+        {
+            // Display  a simple message in the console
+            Console.WriteLine("**** Console Message ****");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            //The Console class is define within the System namespace
+            // Wait for the Enter key before closing the console
+            Console.ReadLine();
+
+            // Return an arbitrary error code
+            return 0;
+        }
+    }
+}
