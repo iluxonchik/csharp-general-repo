4820942b04b299fee8cdd176ae1973ec41062838
iluxonchik <iluxon4ik@hotmail.com>
2014-05-15 15:06:09 +0100

    Chapter 3 Updates.
    Some updates to Chapter 3 code.

--- ProC#5/Chapter 3/SimpleCSharpApp/Program.cs
+++ ProC#5/Chapter 3/SimpleCSharpApp/Program.cs
@@ -12,16 +12,27 @@ namespace SimpleCSharpApp
         //the Main() method is the entry point of the application
         static int Main(string[] args)
         {
-            // Display  a simple message in the console
-            Console.WriteLine("**** Console Message ****");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
+            // Helper method within the Program class
+            ShowEnvironmentDetails();
             //The Console class is define within the System namespace
             // Wait for the Enter key before closing the console
             Console.ReadLine();
 
             // Return an arbitrary error code
             return 0;
+        }
+
+        static void ShowEnvironmentDetails()
+        {
+            // Print out the drives on this machine
+            // as well as some other details
+
+            foreach(string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive : {0}", drive);
+
+            Console.WriteLine("OS : {0}",  Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET version: {0}", Environment.Version);
         }
     }
 }
