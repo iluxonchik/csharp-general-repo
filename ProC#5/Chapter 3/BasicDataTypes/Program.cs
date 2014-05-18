e953c2b8a1c58f31578836aa0860bc765a3051df
iluxonchik <iluxon4ik@hotmail.com>
2014-05-18 10:22:36 +0100

    Added some DateTime, TimeSpan and BigInteger ex.
    Some more examples from Chapter 3 added.

--- ProC#5/Chapter 3/BasicDataTypes/Program.cs
+++ ProC#5/Chapter 3/BasicDataTypes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics; // to use BigInteger
 
 namespace BasicDataTypes
 {
@@ -12,6 +13,8 @@ namespace BasicDataTypes
         {
             LocalVarDeclaration();
             NewingDataTypes();
+            UseDatesAndTimes();
+            UseBigInteger();
             Console.ReadLine();
         }
 
@@ -41,6 +44,44 @@ namespace BasicDataTypes
             double a = new double(); // set to 0
             DateTime dt = new DateTime(); // set to 1/1/0001 12:00:00 AM
             Console.WriteLine("{0}, {1}, {2}, {3}", b, i, a, dt);
+            Console.WriteLine();
+        }
+
+        static void UseDatesAndTimes()
+        {
+            Console.WriteLine("=> Dates and Times:");
+            // This constructor takes (year, month, day)
+            DateTime dt = new DateTime(2010, 10, 17);
+
+            // What day of the month is ths?
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+
+            // The month is now December
+            dt = dt.AddMonths(2);
+            Console.WriteLine("Daylight savings: {0}", dt.IsDaylightSavingTime());
+
+            // This constructor takes (hours, minutes, seconds)
+            TimeSpan ts = new TimeSpan(4, 30, 0);
+            Console.WriteLine(ts);
+
+            // Subtracy 15 mins from the current TimeSpan and print the result
+            Console.WriteLine(ts.Subtract(new TimeSpan(0, 15, 0)));
+            
+            Console.WriteLine();
+        }
+
+        static void UseBigInteger()
+        {
+            Console.WriteLine("=> Use BigInteger:");
+            BigInteger bigInty = BigInteger.Parse("9999999999999999999999999999999999999999999999999999");
+
+            Console.WriteLine("Value of bigInty is {0}", bigInty);
+            Console.WriteLine("Is binInty an even value? {0}", bigInty.IsEven);
+            Console.WriteLine("Is bigInty a power of two? {0}", bigInty.IsPowerOfTwo);
+            BigInteger anEvenBiggerInty = BigInteger.Multiply(bigInty, BigInteger.Parse("888888888888888888888888888888888888888888888"));
+
+            Console.WriteLine("The value of anEvenBiggerInty is {0}", anEvenBiggerInty);
+
             Console.WriteLine();
         }
     }
