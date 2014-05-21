7b2ae356f96acfab140ec2591e2ccafd633c49af
iluxonchik <iluxon4ik@hotmail.com>
2014-05-21 11:48:40 +0100

    Updated on Chapter 3.
    Some updates on Chapter 3, including the checked and unchecked keyword, as
    well as implicit variable declaration examples.

--- ProC#5/Chapter 3/TypeConversions/Program.cs
+++ ProC#5/Chapter 3/TypeConversions/Program.cs
@@ -18,12 +18,17 @@ namespace TypeConversions
 
             short numb3 = 30000, numb4 = 30000;
 
-            // Explicitly cast an int into a short (and allow loss of data)
-            short answer = (short)Add(numb3, numb4);
-
-            Console.WriteLine("{0} + {1} = {2}", numb3, numb4, answer);
+            unchecked
+            {
+                // Explicitly cast an int into a short (and allow loss of data)
+                short answer = (short)Add(numb3, numb4);
+                Console.WriteLine("{0} + {1} = {2}", numb3, numb4, answer);
+            }
+            
 
             NarrowingAttempt();
+            ProcessBytes();
+            DeclareImplicitVars();
 
             Console.ReadLine();
 
@@ -44,6 +49,61 @@ namespace TypeConversions
             // Explicitly cast an into into a byte (no loss of data)
             aByte = (byte)anInt;
             Console.WriteLine("Value of aByte: {0}", aByte);
+        }
+
+        static void ProcessBytes()
+        {
+            byte b1 = 100;
+            byte b2 = 250;
+
+            // Tell the compiler to add CIL code
+            // to throw an exception if overflow/undeflow
+            // occures
+            try
+            {
+                byte sum = checked((byte)Add(b1, b2));
+                Console.WriteLine("sum = {0}", sum);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // Force overflow checking over a block of code statements
+            try
+            {
+                checked
+                {
+                    byte anotherSum = (byte)Add(b1, b2);
+                    Console.WriteLine("sum = {0}", anotherSum);
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // Assuming that /checked is enabled
+            // this block will not trigger a runtime
+            // exception
+            unchecked
+            {
+                byte yetAnotherSum = (byte)(b1 + b2);
+                Console.WriteLine("sum = {0}", yetAnotherSum);
+            }
+        }
+
+        static void DeclareImplicitVars()
+        {
+            // Implicitly typed local varibles
+            var anInt = 0;
+            var aBool = true;
+            var aString = "This is a string!";
+
+            // Print out the underlying data type
+            Console.WriteLine("anInt is a: {0}", anInt.GetType().Name);
+            Console.WriteLine("aBool is a: {0}", aBool.GetType().Name);
+            Console.WriteLine("aString is a: {0}", aString.GetType().Name);
         }
     }
 }
