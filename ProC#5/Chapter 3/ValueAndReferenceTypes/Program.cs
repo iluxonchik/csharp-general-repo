192e24f5082b177ab3fb5bb6bd221da622cfcb65
iluxonchik <iluxon4ik@hotmail.com>
2014-05-28 20:42:04 +0100

    Chpater 4 pass by reference/value additions.
    Some pass by reference/value examples added.

--- ProC#5/Chapter 3/ValueAndReferenceTypes/Program.cs
+++ ProC#5/Chapter 3/ValueAndReferenceTypes/Program.cs
@@ -35,6 +35,38 @@ namespace ValueAndReferenceTypes
         }
     }
 
+    struct Rectangle
+    {
+        // The Rectangle structure contians a reference type member.
+        public ShapeInfo rectInfo;
+
+        public int rectTop, rectLeft, rectBottom, rectRight;
+
+        // Custom constructor.
+        public Rectangle(string info, int top, int left, int bottom, int right)
+        {
+            rectInfo = new ShapeInfo(info);
+            rectTop = top; rectBottom = bottom;
+            rectLeft = left; rectRight = right;
+        }
+
+        public void Display(){
+            Console.WriteLine("String = {0}, Top = {1}, Bottom = {2}, " +
+                 "Left = {3},  Right = {4}", rectInfo.infoString, rectTop, rectBottom, rectLeft, rectRight);
+        }
+    }
+
+    class ShapeInfo
+    {
+        public string infoString;
+
+        // Custom constructor.
+        public ShapeInfo(string info)
+        {
+            infoString = info;
+        }
+    }
+
     //Classes are always reference types.
     class PointRef
     {
@@ -73,6 +105,23 @@ namespace ValueAndReferenceTypes
             ValueTypeAssignment();
             ReferenceTypeAssignment();
             
+            // Create the first rectangle
+            Console.WriteLine("-> Creating r1");
+            Rectangle r1 = new Rectangle("First Rect", 10, 10, 50, 50);
+
+            // Now assign a new Rectangle to r1
+            Console.WriteLine("-> Assigning r2 to r1");
+            Rectangle r2 = r1;
+
+            // Change some values of r2.
+            Console.WriteLine("-> Changing values of r2");
+            r2.rectInfo.infoString = "This is some new info!";
+            r2.rectBottom = 1245;
+
+            // Print both of the rectangles.
+            r1.Display();
+            r2.Display();
+
             Console.ReadLine();
         }
 
