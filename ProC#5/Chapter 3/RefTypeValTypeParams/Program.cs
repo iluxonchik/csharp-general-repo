b6c2e2bec4062a966aff7134eba2c86a79b7ed44
iluxonchik <iluxon4ik@hotmail.com>
2014-05-29 18:51:02 +0100

    Chapter 4 additions.
    Some additions regarding the parameter passing.

---
+++ ProC#5/Chapter 3/RefTypeValTypeParams/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypeValTypeParams
+{
+    class Person
+    {
+        public string personName;
+        public int personAge;
+
+        // Constructors
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+        public Person() {}
+
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Passing Person Object By Value ****");
+            Person fred = new Person("Fred", 12);
+            Console.WriteLine("\nBefore by value call, Person is:");
+            fred.Display();
+
+            SemdAPersonByValue(fred);
+            Console.WriteLine("\nAfter call by value, Person is:");
+            fred.Display();
+
+            Console.ReadLine();
+        }
+
+        static void SemdAPersonByValue(Person p)
+        {
+            p.personAge = 18;
+
+            p = new Person("John", 18);
+        }
+    }
+}
