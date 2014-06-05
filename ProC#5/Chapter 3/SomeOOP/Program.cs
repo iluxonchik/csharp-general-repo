b2a3ac7158687bc810b870514bd171d0cc0fe433
iluxonchik <iluxon4ik@hotmail.com>
2014-06-05 22:10:54 +0100

    Some polymorphism examples added.

--- ProC#5/Chapter 3/SomeOOP/Program.cs
+++ ProC#5/Chapter 3/SomeOOP/Program.cs
@@ -13,6 +13,19 @@ namespace SomeOOP
             // Call to turn the radio on/off is done internally
             Car aCar = new Car();
             aCar.TurnOnRadio(false);
+
+            Shape [] someShapes = new Shape[3];
+            someShapes[0] = new Hexagon();
+            someShapes[1] = new Circle();
+            someShapes[2] = new Hexagon();
+
+            foreach (Shape s in someShapes)
+         		{
+         			// Use the polymorphic interface!
+         			s.Draw();
+         		}
+
+         		Console.ReadLine();
         }
     }
 }
