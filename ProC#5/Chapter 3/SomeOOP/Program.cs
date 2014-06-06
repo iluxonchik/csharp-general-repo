9656e6a3e386abf7f1062b9d287e892791247df9
iluxonchik <iluxon4ik@hotmail.com>
2014-06-06 23:16:24 +0100

    Chapter 5 addtions.
    Some more additions from Chapter 5. Includes some "getters" and "setters".

--- ProC#5/Chapter 3/SomeOOP/Program.cs
+++ ProC#5/Chapter 3/SomeOOP/Program.cs
@@ -14,18 +14,7 @@ namespace SomeOOP
             Car aCar = new Car();
             aCar.TurnOnRadio(false);
 
-            Shape [] someShapes = new Shape[3];
-            someShapes[0] = new Hexagon();
-            someShapes[1] = new Circle();
-            someShapes[2] = new Hexagon();
-
-            foreach (Shape s in someShapes)
-         		{
-         			// Use the polymorphic interface!
-         			s.Draw();
-         		}
-
-         		Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
---
+++ ProC#5/Chapter 3/SomeOOP/SportsCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeOOP
+{
+    public class SportsCar
+    {
+        // Nested types can be marked private
+        private enum CarColor
+        {
+            Red, Green, Blue
+        }
+    }
+}
