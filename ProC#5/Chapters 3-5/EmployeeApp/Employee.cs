8019cb0ca9ca4e1b9120b88c45cb1a38358db4c0
iluxonchik <iluxon4ik@hotmail.com>
2014-06-07 23:57:35 +0100

    Added some encapsulation examples.
    Chapter 5 encapsulation examples added.

---
+++ ProC#5/Chapters 3-5/EmployeeApp/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Field data
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        // Acessor (get method)
+        public string GetName()
+        {
+            return this.empName;
+        }
+
+        // Mutator (set method)
+        public void SetName(string name)
+        {
+            // Perform a check on the incoming
+            // value before making the assignment
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name must be 16 characters or less!");
+            else
+                this.empName = name;
+        }
+        // Constructor
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            this.empName = name;
+            this.empID = id;
+            this.currPay = pay;
+        }
+
+        // Methods
+        public void GiveBonus(float amount)
+        {
+            this.currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", this.empName);
+            Console.WriteLine("ID: {0}", this.empID);
+            Console.WriteLine("Pay: {0}", this.currPay);
+        }
+    }
+}
---
+++ ProC#5/Chapters 3-5/EmployeeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun With Encapsulation ****\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // Use the set/get methods to interact with the object's name
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+
+            Console.ReadLine();
+        }
+    }
+}
