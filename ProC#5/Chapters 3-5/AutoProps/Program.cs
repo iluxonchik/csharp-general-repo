08378ecf1fbc1f9e4ab6bd567cfe30e0c9444d82
iluxonchik <iluxon4ik@hotmail.com>
2014-06-11 23:54:39 +0100

    Automatic properties examples added.
    Some examples with automatic properties added.

---
+++ ProC#5/Chapters 3-5/AutoProps/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Automatic Properties *****\n");
+
+            Car c = new Car();
+            c.PetName = "A Car";
+            c.Speed = 978;
+            c.Color = "Blue";
+
+            Console.WriteLine("Your car is named {0}? Okay then...", c.PetName);
+            c.DispayStats();
+
+            Console.ReadLine();
+        }
+    }
+}
