b138fb8c0e1ab08edbda9debf0465ccb0358405a
iluxonchik <iluxon4ik@hotmail.com>
2014-06-16 23:45:40 +0100

    ConstData project from Chapter 5 added.

---
+++ ProC#5/Chapters 3-5/ConstData/MyMathClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+        // Read-only fields can be assigned in ctors, but nowhere else
+        public readonly double roPI;
+
+        public MyMathClass()
+        {
+            this.roPI = 3.14;
+        }
+    }
+}
