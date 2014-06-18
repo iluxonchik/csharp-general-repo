81e2b332fd646056db27a0f1190328a706af61ba
iluxonchik <iluxon4ik@hotmail.com>
2014-06-18 23:44:42 +0100

    Parser and ExtractionInfo classes added.
    Added an initial (and incomplete) implementation of the 'Parser' class,
    which will process all of the incoming input. A static class added
    ('ExtractionInfo') which for now only contains the zip files path and the
    extraction path. It will also contain the file exention.

---
+++ Projects/ExtractIndividualFiles/ExtractionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractIndividualFiles
+{
+    static class ExtractionInfo
+    {
+        public static string extractionPath;
+        public static string zipPath;
+         
+    }
+}
---
+++ Projects/ExtractIndividualFiles/Parser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExtractIndividualFiles
+{
+    class Parser
+    {   
+        private enum Option
+        {
+            exit,
+            yes,
+            no,
+            error
+        };
+
+        Option userOpt;
+        private bool zipDirSet = false; // tracks if the zip file directory has been set
+        private string optionInput;
+
+        private bool processOpt(string option)
+        {
+            if (option == "yes" || option == "y")
+            {
+                userOpt = Option.yes;
+                return true;
+            }
+            else if (option == "no" || option == "n")
+            {
+                userOpt = Option.no;
+                return true;
+            }
+            else if (option == "exit" || option =="e")
+            {
+                userOpt = Option.exit;
+                return true;
+            }
+
+            return false; // no valid option found
+            
+        }
+
+        public void processInput()
+        {
+            
+            ExtractionInfo.zipPath = Console.ReadLine(); // read the zip path
+
+            while (!File.Exists(ExtractionInfo.zipPath))
+            {
+                Console.WriteLine("Invalid zip file path. Please enter a valid one: ");
+                ExtractionInfo.zipPath = Console.ReadLine();
+            }
+            
+            ExtractionInfo.extractionPath = Console.ReadLine(); // read the extraction path
+
+            if (!Directory.Exists(ExtractionInfo.extractionPath))
+            {
+                Console.WriteLine("The {0} directory does not exist.", ExtractionInfo.extractionPath);
+                Console.WriteLine("Would you like to create it?");
+                
+                OptionInput = Console.ReadLine().ToLower();
+                // Process user's option input
+                while (!processOpt(OptionInput))
+                    // While the input is not valid, keep prompting
+                    Console.WriteLine("Please answer 'yes' or 'no' "); // 'exit' works here too
+                /*  TODO: 
+                 *  + if the user answers 'yes'
+                 *  + if the user answers 'no'
+                 */
+
+            }
+
+            
+        }
+
+        // Properties
+        private string OptionInput { set; get; }
+
+
+
+    }
+
+}
