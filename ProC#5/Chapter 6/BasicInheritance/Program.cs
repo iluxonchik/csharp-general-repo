b613b8844d1a2898b93eb809235d190713c9b2eb
iluxonchik <iluxon4ik@hotmail.com>
2014-06-25 23:54:18 +0100

    Added BasicInheritance from Chapter 6.

---
+++ ProC#5/Chapter 6/BasicInheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            // Make a Car object and set max speed
+            Car aCar = new Car(80);
+
+            // Set the current speed and print it
+            aCar.Speed = 50;
+            Console.WriteLine("The car is going on {0} MPH", aCar.Speed);
+            Console.ReadLine();
+        }
+    }
+}
