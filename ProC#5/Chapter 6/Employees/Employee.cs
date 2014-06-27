48d8e3c9a66b6d70259b4075f70e4a2c29dd4d69
iluxonchik <iluxon4ik@hotmail.com>
2014-06-27 22:32:13 +0100

    Chapter 6 additions.
    Some chapter 6 additions.

---
+++ ProC#5/Chapter 6/Employees/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    partial class Employee
+    {
+        
+        // Propreties!
+        public int Age
+        {
+            get { return this.empAge; }
+            set { this.empAge = value; }
+        }
+        public string Name
+        {
+            get { return this.empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name must be 16 chars or less");
+                else
+                    this.empName = value;
+            }
+        }
+
+        public string SSN
+        {
+            // There is no 'set' method. This property is Read-Only
+            get
+            {return this.empSS;}
+        }
+
+        // The 'int' represents the type of data this proprety encapsulates
+        // The data type must be identical to the related field (this.empID)
+        public int ID
+        {
+            get { return this.empID; }
+            set { this.empID = value; }
+        }
+        public float Pay
+        {
+            get { return this.currPay; }
+            set { this.currPay = value; }
+        }
+
+        // Methods
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", this.Name);
+            Console.WriteLine("ID: {0}", this.ID);
+            Console.WriteLine("Age: {0}", this.Age);
+            Console.WriteLine("Pay: {0}", this.Pay);
+        }
+    }
+}
---
+++ ProC#5/Chapter 6/Employees/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** The Employee Class Hierarchy ****\n");
+            SalesPerson fred = new SalesPerson();
+            fred.Age = 18;
+            fred.Name = "Fred";
+            fred.SalesNumber = 999;
+        }
+    }
+}
