5bca02403a2c08e894748dd29c08c95549d3f995
iluxonchik <iluxon4ik@hotmail.com>
2014-07-07 21:34:40 +0100

    Updated to use multiple threads for requests.
    Now the requests are distributed through various threads. This brings a
    notable speed improvement (depending from the number of threads).

--- Projects/YouTubeVooster/Form1.cs
+++ Projects/YouTubeVooster/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace YouTubeVooster
 {
@@ -26,13 +27,31 @@ namespace YouTubeVooster
             Video video = new Video(txtVideoID.Text);
             //MessageBox.Show(video.VideoToken);
 
+            // NOTE: The number of threads is hardcoded now.
+            // TODO: Ask user for number of threads to use
 
-           
+            // Assuming that there are 20 threads
+            int NUMTHREADS = 20;
+            int userNumViews = int.Parse(txtNumViews.Text);
+            // First let's deliver the remainder of int division
+            int remainder = userNumViews % NUMTHREADS;
+            for (int i = 0; i < remainder; i++)
+                video.playVideo();
+
+            // This player is used in each one of the threads
+            Player newPlayer = new Player(userNumViews/ NUMTHREADS, video);
+
+            // Now, let's distribute the rest of the views through X threads
+            for (int i = 0; i < NUMTHREADS; i++)
+                new Thread(new ThreadStart(newPlayer.makeYTRequest)).Start();
+
+            /************** "Classical" approach*******************
             for (int i = 0; i < int.Parse(txtNumViews.Text); i++)
             {
                 video.playVideo();
                 label5.Text = i.ToString();
             }
+            ******************************************************/
              
         }
     }
---
+++ Projects/YouTubeVooster/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeVooster
+{
+    class Player
+    {
+        protected int numPlays;
+        protected Video video;
+        public Player(int numPlays, Video video)
+        {
+            if (numPlays > 0)
+                this.numPlays = numPlays;
+            if (video != null)
+                this.video = video;
+        }
+
+        // Properties
+        public int NumPlays
+        {
+            get { return this.numPlays; }
+        }
+
+        // Methods
+
+        public void makeYTRequest()
+        {
+            for (int i = 0; i < this.numPlays; i++)
+                this.video.playVideo();
+        }
+    }
+}
