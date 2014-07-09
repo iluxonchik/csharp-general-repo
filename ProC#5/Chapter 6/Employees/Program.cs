2dc8fe4cee1966c4cccae2f1508540d5748747c3
iluxonchik <iluxon4ik@hotmail.com>
2014-07-09 14:55:27 +0100

    Program.cs updated.

--- ProC#5/Chapter 6/Employees/Program.cs
+++ ProC#5/Chapter 6/Employees/Program.cs
@@ -11,10 +11,17 @@ namespace Employees
         static void Main(string[] args)
         {
             Console.WriteLine("***** The Employee Class Hierarchy ****\n");
-            SalesPerson fred = new SalesPerson();
-            fred.Age = 18;
-            fred.Name = "Fred";
-            fred.SalesNumber = 999;
+            
+            Manager chucky = new Manager("Cucky", 50, 92, 100000, "333-23-2322", 9000;
+            chucky.GiveBonus(300);
+            chucky.DisplayStats();
+            Console.WriteLine();
+
+            SalesPerson fran = new SalesPerson("Fran", 43, 93, 300, "932-32-3232", 31);
+            fran.GiveBonus(200);
+            fran.DisplayStats();
+            Console.ReadLine();
+        
         }
     }
 }
