77e19b2052d74a564358b5a80cb8b91fedbb2bef
iluxonchik <iluxon4ik@hotmail.com>
2014-07-12 23:52:04 +0100

    Member Shadowing examples added

---
+++ Projects/Shapes/ThreeDCircle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class ThreeDCircle : Circle
+    {
+        // Hide the PetName property above me
+        public new string PetName { get; set; }
+        // Hide any Draw() implementation above me (Member Shadowing)
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing a 3D Circle");
+        }
+    }
+}
