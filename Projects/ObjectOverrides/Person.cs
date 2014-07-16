05806efad7a0322738a62c35fdf01f063c101321
iluxonchik <iluxon4ik@hotmail.com>
2014-07-16 22:21:51 +0100

    More method overrides additions.

--- Projects/ObjectOverrides/Person.cs
+++ Projects/ObjectOverrides/Person.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace ObjectOverrides
 {
+    // Person extends Object
     class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+        public Person() {}
+
+        // Overring System.Object.ToString()
+        public override string ToString()
+        {
+            string state;
+            state = string.Format("[First Name: {0}; Last Name: {1}; Age: {2}", FirstName, LastName, Age);
+            return state;
+        }
+
+        // Overriding System.Obejct.Equals()
+        public override bool Equals(object obj)
+        {
+            // No need to cast "obj" to a Person,
+            // since EVERYTHING HAS A ToString() method
+            return obj.ToString() == this.ToString();
+        }
+
     }
 }
