3828a7073d9f25328c69bdb5f09ad5d0d3a6f7d4
iluxonchik <iluxon4ik@hotmail.com>
2014-07-18 22:45:39 +0100

    ObjectOverrides project finished. Chapter 6 finished.

--- Projects/ObjectOverrides/Person.cs
+++ Projects/ObjectOverrides/Person.cs
@@ -44,5 +44,14 @@ namespace ObjectOverrides
             return obj.ToString() == this.ToString();
         }
 
+        static void StaticMembersOfObject()
+        {
+            // Static members of System.Object
+            Person p3 = new Person("Sally", "Jones", 4);
+            Person p4 = new Person("Sally", "Jones", 4);
+            Console.WriteLine("P3 and P4 have same state: {0}", object.Equals(p3, p4));
+            Console.WriteLine("P3 and P4 are pointing to the same object: {0}", object.ReferenceEquals(p3, p4));
+        }
+
     }
 }
--- Projects/ObjectOverrides/Program.cs
+++ Projects/ObjectOverrides/Program.cs
@@ -11,21 +11,30 @@ namespace ObjectOverrides
         static void Main(string[] args)
         {
             Console.WriteLine("*** Fun With System.Object\n ***");
-            Person p1 = new Person();
+            // NOTE: Those gotta be indentical to test the
+            // Equals() and GetHash() methods
+            Person p1 = new Person("Jason", "Meow", 18);
+            Person p2 = new Person("Jason", "Meow", 18);
 
-            // Use inherited members of System.Object
-            Console.WriteLine("ToString: {0}", p1.ToString());
-            Console.WriteLine("Hash Code: {0}", p1.GetHashCode());
-            Console.WriteLine("Type: {0}", p1.GetType());
+            // Get stringified versions of objects
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
 
-            // Make some other references to p1
-            Person p2 = p1;
-            object o = p2;
+            // Test the overridden Equals()
+            Console.WriteLine("p1 = p2? : {0}", p1.Equals(p2));
 
-            // Are the references pointing to the same object in memorty?  (YES!)
-            if (o.Equals(p1) && p2.Equals(o))
-                Console.WriteLine("Same instance!");
-            
+            // Test hash codes
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+
+            // Change the age of p2 and test again
+            p2.Age = 19;
+
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+            Console.WriteLine("p1 = p2? : {0}", p1.Equals(p2));
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+
+
             Console.ReadLine();
         }
     }
