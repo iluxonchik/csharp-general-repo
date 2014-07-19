75e60c6f99f96e6bb57044d7a77a63c411a2774c
iluxonchik <iluxon4ik@hotmail.com>
2014-07-19 15:54:47 +0100

    SimpleException from Chapter 7 added.
    No actual exceptions added yet. Next commit should contain them.

---
+++ ProC#5/Chapter 7/SimpleException/Car..cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Constant for maximum speed
+        public const int MaxSpeed = 100;
+
+        // Car properties
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car operational?
+        private bool carIsDead;
+
+        // A car has-a radio
+        private Radio theMusicBox = new Radio();
+
+        // Constructors
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object
+            theMusicBox.TurnOn(state);
+        }
+
+        // See if Car has overheadred
+       public void Accelerate (int delta)
+       {
+           if (carIsDead)
+               Console.WriteLine("{0} is out of order...", PetName);
+           else
+           {
+               CurrentSpeed += delta;
+               if (CurrentSpeed > MaxSpeed)
+               {
+                   Console.WriteLine("{0} has overheated!", PetName);
+                   CurrentSpeed = 0;
+                   carIsDead = true;
+               }
+               else
+                   Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+           }
+       }
+
+    }
+}
