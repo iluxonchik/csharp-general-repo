d4c7f1ccdf3f8a196d5fc1c2965be747a619d7f5
iluxonchik <iluxon4ik@hotmail.com>
2014-07-23 23:58:17 +0100

    Custom Exception example added.

---
+++ ProC#5/Chapter 7/CustomException/Car..cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Car
+    {
+        // Constant for maximum speed
+        public const int MaxSpeed = 100;
+
+        // Car properties
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car operational?
+        private bool carIsDead;
+
+        // A car has-a radio
+        private Radio theMusicBox = new Radio();
+
+        // Constructors
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object
+            theMusicBox.TurnOn(state);
+        }
+
+        // See if Car has overheadred
+       public void Accelerate (int delta)
+       {
+           if (carIsDead)
+               Console.WriteLine("{0} is out of order...", PetName);
+           else
+           {
+               CurrentSpeed += delta;
+               if (CurrentSpeed > MaxSpeed)
+               {
+                   CurrentSpeed = 0;
+                   carIsDead = true;
+
+                  // We need to call the HelpLink property,  thus we need
+                  // to create a local variable before throwing the Exception object
+                   CarIsDeadException ex = new CarIsDeadException(string.Format("{0} has overheated!", PetName), "You have a lead foot", DateTime.Now);
+                   ex.HelpLink = "http://www.google.co.uk";
+                   throw ex;
+
+               }
+               else
+                   Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+           }
+       }
+
+    }
+}
---
+++ ProC#5/Chapter 7/CustomException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Custom Exceptions *****\n");
+            Car aCar = new Car("Rusty", 90);
+            try
+            {
+                // Trip exception
+                aCar.Accelerate(50);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
