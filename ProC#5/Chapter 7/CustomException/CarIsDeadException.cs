ebede3518edd0c931289ad20d2a0873aa24443c5
iluxonchik <iluxon4ik@hotmail.com>
2014-07-24 22:26:13 +0100

    CutomException project from Chapter 8 finished,

--- ProC#5/Chapter 7/CustomException/CarIsDeadException.cs
+++ ProC#5/Chapter 7/CustomException/CarIsDeadException.cs
@@ -6,27 +6,25 @@ using System.Threading.Tasks;
 
 namespace CustomException
 {
+    [Serializable]
     public class CarIsDeadException : ApplicationException
     {
-        private string messageDetails = String.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
-        public CarIsDeadException(string message, string cause, DateTime time)
+        public CarIsDeadException(string message) : base(message) { }
+        public CarIsDeadException(string message, System.Exception inner) : base(message, inner) { }
+        protected CarIsDeadException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+        public CarIsDeadException(string message, string cause, DateTime time) 
+            :base(message)
         {
-            messageDetails = message;
             CauseOfError = cause;
             ErrorTimeStamp = time;
         }
 
-        // Override the Exception.Message property
-        public override string Message
-        {
-            get
-            {
-                return string.Format("Car Error Message: {0}",messageDetails);
-            }
-        }
+        
     }
 }
