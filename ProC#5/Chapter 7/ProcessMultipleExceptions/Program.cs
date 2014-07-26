6112152ed57aeecdfabf51bd9eac44a8d3a228eb
iluxonchik <iluxon4ik@hotmail.com>
2014-07-26 23:50:26 +0100

    More Exceptions examples added.

--- ProC#5/Chapter 7/ProcessMultipleExceptions/Program.cs
+++ ProC#5/Chapter 7/ProcessMultipleExceptions/Program.cs
@@ -12,17 +12,40 @@ namespace ProcessMultipleExceptions
         {
             Console.WriteLine("***** Handling Multiple Exceptions *****\n");
             Car aCar = new Car("Rusty", 90);
+
             try
             {
+                // Trigger an argument out of range exception
                 aCar.Accelerate(-10);
             }
             catch (CarIsDeadException e)
             {
-                  Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message);
+                // rethrow the exception up the stack to the previous caller
+                // NOTE: in this example code, the ultimate receiver of CarIsDeadException is the CLR, because
+                // it is the Main() method rethrowing the exception.
+                throw;
             }
             catch (ArgumentOutOfRangeException e)
             {
                 Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                // This will catch any other exceprion
+                // beyond CarIsDeadException or
+                // ArgumentOutOfRangeException
+                Console.WriteLine(e.Message);
+            }
+
+            // General exception example
+            try
+            {
+                aCar.Accelerate(-100);
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong...");
             }
 
             Console.ReadLine();
@@ -31,4 +54,4 @@ namespace ProcessMultipleExceptions
 
         }
     }
-}
+
