81369ec25dd91beb49022e6a5bf7c730833340b4
iluxonchik <iluxon4ik@hotmail.com>
2014-07-28 22:03:32 +0100

    Interface examples added.

---
+++ ProC#5/Chapter 8/CustomInterface/IPointy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    // This interface defines the behaviour of "having points"
+    interface IPointy
+    {
+        // A read-write property behavior as a read-only property
+        // retType PropName { get; set; }
+        // Below is an example of a read-only property
+        byte Points { get; }
+    }
+}
