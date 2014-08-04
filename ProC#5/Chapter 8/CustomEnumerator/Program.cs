2039c65901f701e38b3fabd238c03131ffb3b84e
iluxonchik <iluxon4ik@hotmail.com>
2014-08-04 20:44:47 +0100

    CustomEnumerator project added.

---
+++ ProC#5/Chapter 8/CustomEnumerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage carLot = new Garage();
+
+            // Hand over each car in the collection
+            foreach (Car c in carLot)
+            {
+                Console.WriteLine("{0} is going at {1} MPH", c.PetName, c.CurrentSpeed);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
