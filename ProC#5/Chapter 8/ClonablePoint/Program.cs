67d4bde5202e9899ba79d3ffd794232cdb6ed0e9
iluxonchik <iluxon4ik@hotmail.com>
2014-08-05 23:50:01 +0100

    ClonablePoint project added. Point class added.@

---
+++ ProC#5/Chapter 8/ClonablePoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClonablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+
+        // Override Object.ToString()
+        public override string ToString()
+        {
+            return string.Format("X = {0}; Y = {1}", X, Y);
+        }
+    }
+}
