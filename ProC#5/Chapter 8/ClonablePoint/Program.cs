41a422c04bd156b63ae4cc1c2662cb8a1e945b71
iluxonchik <iluxon4ik@hotmail.com>
2014-08-06 23:51:41 +0100

    Object cloning example added.

--- ProC#5/Chapter 8/ClonablePoint/Program.cs
+++ ProC#5/Chapter 8/ClonablePoint/Program.cs
@@ -10,10 +10,29 @@ namespace ClonablePoint
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun With Object Cloning *****\n");
+            // Two referrences pointing to the same object
+            Point p1 = new Point(50, 50);
+            Point p2 = p1;
+            p2.X = 0;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            Point p3 = new Point(100, 100);
+            Point p4 = (Point)p3.Clone();
+
+            // Change p4.X, which will not change p3.X, since p3 was cloned
+            p4.X = 421;
+
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+
+            Console.ReadLine();
         }
     }
 
-    public class Point
+    public class Point: ICloneable
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -25,6 +44,14 @@ namespace ClonablePoint
         public override string ToString()
         {
             return string.Format("X = {0}; Y = {1}", X, Y);
+        }
+
+        // Return a copy of the current object
+        public object Clone()
+        {  
+            // Copy each field of the Point, member by member
+            return this.MemberwiseClone();
+            // return new Point(this.X, this.Y); would work too! 
         }
     }
 }
