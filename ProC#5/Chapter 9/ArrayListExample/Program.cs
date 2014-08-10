d42427d4783b40ec87c676c84ee0a6583d0c1d35
iluxonchik <iluxon4ik@hotmail.com>
2014-08-10 23:32:54 +0100

    Added an ArrayList example.

---
+++ ProC#5/Chapter 9/ArrayListExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList strArray = new ArrayList();
+            strArray.AddRange(new string[] { "First", "Second", "Third" });
+            Console.WriteLine("{0} items\n", strArray.Count);
+
+            // Add a new item and display the count
+            strArray.Add("Fourth!");
+            Console.WriteLine("Now there are {0} items! \n", strArray.Count);
+
+            foreach(string s in strArray)
+                Console.WriteLine("Entry: {0}", s);
+
+            Console.ReadLine();
+        }
+
+      
+    }
+}
