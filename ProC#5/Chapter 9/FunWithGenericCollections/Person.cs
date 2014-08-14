e8550cd6f34495d8fdc79d349de9df060c829a69
iluxonchik <iluxon4ik@hotmail.com>
2014-08-14 23:53:51 +0100

    List<T> examples added.

---
+++ ProC#5/Chapter 9/FunWithGenericCollections/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericCollections
+{
+    // Person extends Object
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string SSN { get; set; }
+
+        // Return a hash code based on a point unique string data
+        public override int GetHashCode()
+        {
+            return SSN.GetHashCode();
+        }
+
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+        public Person() {}
+
+        // Overring System.Object.ToString()
+        public override string ToString()
+        {
+            string state;
+            state = string.Format("[First Name: {0}; Last Name: {1}; Age: {2}", FirstName, LastName, Age);
+            return state;
+        }
+
+        // Overriding System.Obejct.Equals()
+        public override bool Equals(object obj)
+        {
+            // No need to cast "obj" to a Person,
+            // since EVERYTHING HAS A ToString() method
+            return obj.ToString() == this.ToString();
+        }
+
+        static void StaticMembersOfObject()
+        {
+            // Static members of System.Object
+            Person p3 = new Person("Sally", "Jones", 4);
+            Person p4 = new Person("Sally", "Jones", 4);
+            Console.WriteLine("P3 and P4 have same state: {0}", object.Equals(p3, p4));
+            Console.WriteLine("P3 and P4 are pointing to the same object: {0}", object.ReferenceEquals(p3, p4));
+        }
+
+    }
+}
---
+++ ProC#5/Chapter 9/FunWithGenericCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void UseGenericList()
+        {
+            // Make a List of Person objects, filled with
+            // collection/object init syntax
+            List<Person> people = new List<Person>()
+            {
+                new Person {FirstName = "John", LastName="Terry", Age= 30},
+                new Person {FirstName = "Frank", LastName = "Lampard", Age= 18},
+                new Person {FirstName = "Petr", LastName = "Cech", Age= 19}
+            };
+
+            // Print the number of items in the list
+            Console.WriteLine("Items in the list: {0}", people.Count);
+
+            // Enumerate over the list
+            foreach (Person p in people)
+                Console.WriteLine(p);
+
+            // Insert a new Person
+            Console.WriteLine("\n-> Inserting a new person...");
+            people.Insert(2, new Person { FirstName = "Fernando", LastName = "Torres", Age = 20 } );
+            Console.WriteLine("Items in list: {0}", people.Count);
+
+            // Copy data into a new array
+            Person[] arrOfPeople = people.ToArray();
+            for (int i = 0; i < arrOfPeople.Length; i++)
+                Console.WriteLine("First Name: {0}", arrOfPeople[i].FirstName);
+        }
+    }
+}
