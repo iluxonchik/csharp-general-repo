7247721113c1757483e76714b182e347bc1dfb16
iluxonchik <iluxon4ik@hotmail.com>
2014-08-27 23:50:27 +0100

    Car class added (basic implementation).

---
+++ ProC#5/Chapter 10/CarDelegate/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        // Internal state data
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car alive?
+        private bool carIsDead;
+
+        // Class constructors
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+    }
+}
