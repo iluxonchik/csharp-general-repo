f5a6a002b595cb1edb28756d93b7fe014605924f
iluxonchik <iluxon4ik@hotmail.com>
2014-08-28 18:26:43 +0100

    Car delegate added.

--- ProC#5/Chapter 10/CarDelegate/Car.cs
+++ ProC#5/Chapter 10/CarDelegate/Car.cs
@@ -8,6 +8,53 @@ namespace CarDelegate
 {
     public class Car
     {
+
+        // 1) Define a delegate type
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // 2) Define a member variable of this delegate
+        private CarEngineHandler listOfHandlers;
+
+        // 3) Add a registration function for the caller
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+
+        // 4) Implemenet the Accelerate() method to invoke the delegate's
+        //    invocation list under the correct circumstances.
+        public void Accelerate(int delta)
+        {
+            // If the car is "dead", send a dead message
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("Sorry, the car is dead...");
+                }
+                else
+                {
+                    CurrentSpeed += delta;
+
+                    // Is the car "almost dead"?
+                    if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                    {
+                        listOfHandlers("Careful, it's gonna blow up!");
+                    }
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+
+
         // Internal state data
         public int CurrentSpeed { get; set; }
         public int MaxSpeed { get; set; }
