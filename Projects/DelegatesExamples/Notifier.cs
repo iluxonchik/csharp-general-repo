6838faacb7dfdbb4ba3ebd28d13f3579ec3c2155
iluxonchik <iluxon4ik@hotmail.com>
2014-09-03 05:22:38 +0100

    Delegate examples added.

---
+++ Projects/DelegatesExamples/Notifier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExamples
+{
+    class Notifier
+    {
+
+        private int count;
+        
+        public int Count 
+        {
+            set
+            {this.count = value;}
+
+            get { return this.count; }
+        }
+
+        public delegate void Notify(string msg);
+
+        private Notifier.Notify listOfClients;
+
+
+        public Notifier(int countValue)
+        {
+            Count = countValue;
+        }
+
+        public void Test()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            if (listOfClients != null)
+            {
+                listOfClients( Count.ToString() + " seconds have passed!");
+            }
+        }
+
+        public void RegisterAMethod(Notify methodToCall)
+        {
+            listOfClients += methodToCall;
+        }
+
+        public void UnregisterAMethod(Notify methodToCall)
+        {
+            listOfClients -= methodToCall;
+        }
+    }
+}
---
+++ Projects/DelegatesExamples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This here is to be able to add non-static methods to the list of clients
+            Program p1 = new Program(); 
+
+
+            Notifier n1 = new Notifier(2);
+            
+            // Create some delegate objects
+            Notifier.Notify delegate1 = new Notifier.Notify(p1.Method1);
+            Notifier.Notify delegate2 = new Notifier.Notify(p1.Method2);
+            Notifier.Notify delegate3 = new Notifier.Notify(p1.Method3);
+
+            n1.RegisterAMethod(delegate1);
+            n1.RegisterAMethod(delegate2);
+            n1.RegisterAMethod(delegate3);
+
+            n1.Test();
+
+            // "Method2" removed from the list of clients
+            // it will not report back anymore.
+            n1.UnregisterAMethod(delegate2);
+
+            n1.Test();
+
+            Console.ReadLine();
+
+        }
+
+
+        // Just some methods to register/unregister
+        public void Method1(string msg)
+        {
+            Console.WriteLine("\n Method1: {0}", msg);
+        }
+
+        public void Method2(string msg)
+        {
+            Console.WriteLine("\n Method2: {0}", msg);
+        }
+
+        public void Method3(string msg)
+        {
+            Console.WriteLine("\n Method3: {0}", msg);
+        }
+    }
+}
