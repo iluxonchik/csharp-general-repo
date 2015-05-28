7f15fd16c431189b8804d103563d048fda74e6bb
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-05-28 06:38:50 +0100

    CarEvents project added.

---
+++ ProC#5/Chapter 10/CarEvents/CarEvents/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        // This delegate works in conjuction with the Car's events
+        public delegate void CarEngineHandler(string msg);
+
+        // This car can send these events
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+        
+        public void Accelerate(int delta)
+        {
+            // If the car is "dead", send a dead message
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                {
+                    Exploded("Sorry, the car is dead...");
+                }
+            }
+            else
+                {
+                    CurrentSpeed += delta;
+
+                    // Is the car "almost dead"?
+                    if (10 == (MaxSpeed - CurrentSpeed) && AboutToBlow != null)
+                    {
+                        AboutToBlow("Careful, it's gonna blow up!");
+                    }
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            
+        }
+
+
+        // Internal state data
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car alive?
+        private bool carIsDead;
+
+        // Class constructors
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+    }
+}
---
+++ ProC#5/Chapter 10/CarEvents/CarEvents/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Events *****");
+            Car c1 = new Car("Vroom", 100, 10);
+            c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+            c1.AboutToBlow += CarIsAlmostDoomed; // Group conversion syntax
+
+            Car.CarEngineHandler d = new Car.CarEngineHandler(CarExoloded);
+            c1.Exploded += d;
+
+            for(int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            c1.Exploded -= d;
+
+            for(int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+        }
+
+        public static void CarAboutToBlow(string msg) { Console.WriteLine(msg); }
+        public static void CarIsAlmostDoomed(string msg) { Console.WriteLine("=> Critical: {0}", msg); }
+        public static void CarExoloded(string msg) { Console.WriteLine(msg); }
+    }
+}
