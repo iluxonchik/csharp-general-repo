e1514295d685b31437e3385327b63d86636777b2
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-04 23:21:57 +0100

    Anonymous methods example added.

---
+++ ProC#5/Chapter 10/AnonymousMethods/AnonymousMethods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        delegate void SomeDelegate();
+        static void Main(string[] args)
+        {
+
+            Car c1 = new Car("car1", 100, 10);
+            int cnt = 0;
+
+            // Register event handlers as anonymous methods
+            c1.AboutToBlow += delegate
+            {
+                Console.WriteLine("Bout to blow, brah!");
+            };
+
+            c1.AboutToBlow += delegate (object sender, CarEventArgs e)
+            {
+                cnt++;
+                Console.WriteLine("Message from Car: {0}", e.msg);
+            };
+
+            c1.Exploded += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine("Fatal message from Car: {0}", e.msg);
+            };
+
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            // Unecessarily complex code
+            SomeDelegate sd = delegate { Console.WriteLine("Called {0} times", cnt); };
+            sd.Invoke();
+
+            Console.ReadLine();
+        }
+    }
+}
