73a655439650821ae0161e9da5cec919e3df36fa
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-05 21:33:53 +0100

    SimpleIndexer project added.

---
+++ ProC#5/Chapter 11/SimpleIndexer/SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        public int Count
+        {
+            get
+            {
+                return arPeople.Count;
+            }
+        }
+        // Custom indexer for this class
+        public Person this[int index]
+        {
+            get
+            {
+                return (Person)arPeople[index];
+            }
+            set
+            {
+                arPeople.Insert(index, value);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProC#5/Chapter 11/SimpleIndexer/SimpleIndexer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonCollection people = new PersonCollection();
+
+            // Add objects with indexer syntax
+            people[0] = new Person() { Name = "Jason" };
+            people[1] = new Person() { Name = "Jane" };
+            people[2] = new Person() { Name = "Robert" };
+            people[3] = new Person() { Name = "Katie" };
+
+            for (int i = 0; i < people.Count; i++)
+                Console.WriteLine(people[i].Name);
+        }
+    }
+}
