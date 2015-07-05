8b1ea459ec8c5d05b88eb8179a1792c583f3c5fd
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-05 23:03:30 +0100

    ExtensionMethods project added,

---
+++ ProC#5/Chapter 11/ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ExtensionMethods
+{
+    static class MyExtensions
+    {
+        // All objects will be able to use this method (this "object")
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here => {1}\n", obj.GetType().Name,
+                Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        // All ints will be able to use this method (this "int")
+        public static int ReverseDigits(this int i)
+        {
+            char[] digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+
+            return Int32.Parse(new String(digits));
+        }
+    }
+}
---
+++ ProC#5/Chapter 11/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12345;
+            i.DisplayDefiningAssembly();
+
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            Console.WriteLine(i.ReverseDigits());
+        }
+    }
+}
