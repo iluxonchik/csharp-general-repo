aaedea7de05c5d55717b20234cfdceb4f32bab0e
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-06 01:54:47 +0100

    UnsafeCode project added (partiaL).

---
+++ ProC#5/Chapter 11/UnsafeCode/UnsafeCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 20;
+            SafeSwap(ref i, ref j);
+            Console.WriteLine("{0}, {1}", i, j);
+            unsafe { UnsafeSwap(&i, &j); }
+            Console.WriteLine("{0}, {1}", i, j);
+        }
+
+        unsafe public static void UnsafeSwap(int* i, int* j)
+        {
+            int temp = *i;
+            *i = *j;
+            *j = temp;
+        }
+
+        public static void SafeSwap(ref int i, ref int j)
+        {
+            int temp = i;
+            i = j;
+            j = temp;
+        }
+    }
+}
